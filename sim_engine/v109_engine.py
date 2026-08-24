fecf95e5c340691d986911992e81d82522b72a9a
Shaxjan <144225511+Shaxjan@users.noreply.github.com>
2026-08-24 17:47:09 +0700

    v1.0.9: add engine class

---
+++ sim_engine/v109_engine.py
@@ -0,0 +1,7 @@
+from v108_engine import WorldV108
+from v109_runtime import V109RuntimeMixin
+
+
+class WorldV109(V109RuntimeMixin, WorldV108):
+    """v1.0.9: authoritative session read-model pending projection repair."""
+    pass
